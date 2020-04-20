5df3105bc5fcdf53b82078a4856e17c3c09e9380
ProgOwer <ProgOwer0770@gmail.com>
2020-04-20 18:41:44 +0200

    Add Burgers List Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { ApiModule } from './api';
+import { BurgerListComponent } from './burger-list/burger-list.component';
+import { BurgerDetailComponent } from './burger-detail/burger-detail.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerListComponent,
+    BurgerDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApiModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/burger-list/burger-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { BurgersService } from '../api/api/burgers.service';
+
+@Component({
+  selector: 'app-burger-list',
+  templateUrl: './burger-list.component.html',
+  styleUrls: ['./burger-list.component.css']
+})
+export class BurgerListComponent implements OnInit {
+
+  burgers;
+
+  constructor(private burgersService : BurgersService) { }
+
+  ngOnInit(): void {
+    this.getBurgers();
+  }
+
+  getBurgers() : void {
+    this.burgers = this.burgersService.listBurgers()
+        .subscribe(burgers => this.burgers = burgers);
+  }
+
+}
